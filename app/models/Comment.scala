08cdc296e00da2bf3a970e058232a748e9d13d91
Ricky Elrod <ricky@elrod.me>
2013-02-17 07:04:43 -0500

    Add the basis of a comment system.

---
+++ app/models/Comment.scala
@@ -0,0 +1,34 @@
+package models
+
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import play.api.Play.current
+import java.util.Date
+import org.joda.time.{DateTime, Period}
+import org.joda.time.format.{DateTimeFormat, PeriodFormat}
+
+import me.elrod.qsltagger.QSLTaggerUserService
+
+case class Comment(id: Long, cardID: Long, comment: String, date: Date)
+
+object Comment {
+  val simple = {
+    get[Long]("id") ~
+    get[Long]("card_id") ~
+    get[String]("comment") ~
+    get[Date]("commented_at") map {
+      case id ~ cardID ~ comment ~ date => Comment(id, cardID, comment, date)
+    }
+  }
+
+  val withUsers = (Comment.simple) ~ (QSLTaggerUserService.simple) map {
+    case comment ~ user => (comment, user)
+  }
+
+  def getForCardID(cardID: Long) = DB.withConnection { implicit c =>
+    SQL("select a.*, b.* from card_comments a, users b where a.card_id={card_id} and b.id=a.user_id").on(
+      'card_id -> cardID
+    ).as(Comment.withUsers *)
+  }
+}
--- app/models/QSLCard.scala
+++ app/models/QSLCard.scala
@@ -8,24 +8,14 @@ import java.util.Date
 import org.joda.time.{DateTime, Period}
 import org.joda.time.format.{DateTimeFormat, PeriodFormat}
 
-case class QSLCardSide(id: Long, cardID: Long, filename: String) {
-  lazy val card: QSLCard = DB.withConnection { implicit c =>
-    SQL("SELECT * FROM cards WHERE id={card_id}").on(
-      'card_id -> cardID
-    ).as(QSLCard.simple.single)
-  }
-
-  def route = controllers.routes.Assets.at("qslcards/" + filename + ".JPG")
-  def thumbnailRoute = controllers.routes.Assets.at("qslcards_thumbs/" + filename + ".JPG")
-
-}
-
 case class QSLCard(id: Long, approved: Boolean) {
   lazy val sides: List[QSLCardSide] = DB.withConnection { implicit c =>
     SQL("SELECT * FROM card_sides WHERE card_id={card_id}").on(
       'card_id -> id
     ).as(QSLCardSide.simple *)
   }
+
+  lazy val comments = Comment.getForCardID(id)
 }
 
 object QSLCard {
@@ -53,19 +43,5 @@ object QSLCard {
     SQL("SELECT * FROM cards WHERE id={id}").on(
       'id -> id
     ).as(QSLCard.simple.single)
-  }
-}
-
-object QSLCardSide {
-  val simple = {
-    get[Long]("id") ~
-    get[Long]("card_sides.card_id") ~
-    get[String]("card_sides.filename") map {
-      case id ~ cardID ~ filename => QSLCardSide(id, cardID, filename)
-    }
-  }
-
-  lazy val allSides = DB.withConnection { implicit c =>
-    SQL("SELECT t1.* FROM card_sides t1 JOIN (SELECT MAX(id) AS id, card_id, filename FROM card_sides GROUP BY card_id) t2 ON t1.id = t2.id").as(QSLCardSide.simple *)
   }
 }
