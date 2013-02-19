e5ba18f53c76877cf7c656868f8767622aa12c92
Ricky Elrod <ricky@elrod.me>
2013-02-18 21:49:16 -0500

    Nuke overriding Global.

--- app/Global.scala
+++ app/Global.scala
@@ -6,7 +6,3 @@ package object globals {
   val gitCommit = Seq("git", "rev-parse", "--short", "HEAD").!!.trim
   val hostname = java.net.InetAddress.getLocalHost.getHostName
 }
-
-object Global extends GlobalSettings {
-
-}
